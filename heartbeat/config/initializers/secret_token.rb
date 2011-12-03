b14be1a2593e2776064b5956a5c77778c4e5395e
Hans-Gunther Schmidt <hans@otype.de>
2011-12-03 20:16:25 +0100

    adding rails 3.1.3 skeleton

---
+++ heartbeat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heartbeat::Application.config.secret_token = '4cd0adc39878f4ff8efcccb9684bba2ebb7d748357931cb1c7fa0a93fc1453482e9903ac218012a599c5959b3dddc9c825f6eb40e087eaaa78061bbfabdf1e78'
