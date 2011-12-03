f2ac7dd0ca0839ac8731280c2c999e26e1ecc7ea
jaseg <s@jaseg.de>
2011-12-03 21:45:34 +0100

    and removed.

--- sinatra/notification_api.rb
+++ sinatra/notification_api.rb
@@ -4,27 +4,14 @@ require 'sinatra'
 require 'heartbeat'
 MongoMapper.connection = Mongo::Connection.new
 MongoMapper.database = "heartbeat"
-require 'digest/sha1'
-#require 'haml'
-require 'sinatra-authentication'
-use Rack::Session::Cookie, :secret => 'PWUaNDEMp684wCppWY96XBACpWpuoRIG'
 
 set :public_folder, File.dirname(__FILE__) + '/../layout'
-set :sinatra_authentication_view_path, File.dirname(__FILE__) + "/authentication/lib/views/"
-module Sinatra
-  module Helpers
-    def use_layout?
-      false
-    end
-  end
-end
 
 get '/' do
   redirect to '/index.html'
 end
 
 post '/notification' do
-  login_required
 	nf = Notification.new
 	nf.content = params[:content]
 	nf.coordinates = Coordinates.new
