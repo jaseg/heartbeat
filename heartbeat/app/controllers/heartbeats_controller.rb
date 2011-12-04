22ebc769ca6708a7b5678b6129dd578ccf8d5592
Sebastian Gärtner <sebi@kreaktivisten.org>
2011-12-04 14:06:45 +0100

    bootstrap eingebunden

--- heartbeat/app/controllers/heartbeats_controller.rb
+++ heartbeat/app/controllers/heartbeats_controller.rb
@@ -1,6 +1,9 @@
 # -*- encoding : utf-8 -*-
 class HeartbeatsController < ApplicationController
   def index
-    @heartbeats = ["bla", "blub"]
+    @heartbeats = Heartbeat.new
+    @heartbeats.status = 0
+    @heartbeats.timestamp = "2011-12-04T11:32:31.267Z"
+    @heartbeats.source = "mobile"
   end
 end
